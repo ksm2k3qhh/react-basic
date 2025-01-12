bcbbe1a8fbc09d1ec03059004f9eaff021542c6b
ChienNgoUEH <ngochienntph@gmail.com>
2025-01-12 11:16:50 +0700

    video 7

--- src/views/examples/MyComponent.js
+++ src/views/examples/MyComponent.js
@@ -12,15 +12,21 @@ class MyComponent extends React.Component {
             channel: e.target.value,
         })
     }
+    HandleClick = (e) => {
+        alert('click me')
+    }
     render() {
         return (
             <>
                 <div className="first">
-                    <input type="text" value={this.state.name} onChange={(e) => this.HandleOnChange(e)}></input>
+                    <input type="text" onChange={(e) => this.HandleOnChange(e)}></input>
                     My name is : {this.state.name};
                 </div>
                 <div className="second">
                     My channel is : {this.state.channel};
+                </div>
+                <div className="third">
+                    <button onClick={(e) => this.HandleClick(e)}>Click me</button>
                 </div>
             </>
         )
